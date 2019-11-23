bbeef49e48667521d76e5c2f824e6c69fe64c488
ugeHidalgo <ugeHidalgo@hotmail.com>
2019-11-23 08:35:35 +0100

    Added bank accounts service and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BankAccountsComponent } from './components/main/bank-accounts/bank-accounts.component';
+import { GlobalsService } from './globals/globals.service';
+import { BankAccountsService } from './services/bankAccounts/bank-accounts.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BankAccountsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    GlobalsService,
+    BankAccountsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/globals/globals.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GlobalsService {
+
+  server = 'https://apimowizz2ehh.azurewebsites.net/';  //To be used when API is in production
+  //server = 'http://192.168.1.106:44351/';  //To be used when API is in development mode and in local network server.
+  //server = 'https://localhost:5001/';  // To be used when API is in development mode and without connection to any network
+
+  constructor() { }
+}
